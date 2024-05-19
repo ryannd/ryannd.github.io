2e5412f21806a8b557afb57caef96fd723709ccc
ryannd <ryan2dimaranan@gmail.com>
2024-05-19 17:42:16 -0500

    :sparkles: (menu) mobile menu

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,17 +1,26 @@
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 import Menu from "./menu";
+import MobileMenu from "./mobile_menu";
+
 
 export default function Layout({ children }: { children?: ReactNode }) {
+    const [isMenuOpen, setMenuOpen] = useState(false);
+
+    const onMenuClick = () => {
+      setMenuOpen(!isMenuOpen);
+    }
+
     return <>
-    <main className="p-10 w-screen h-screen">
-      <div className="lg:grid lg:grid-cols-3 h-full gap-8">
-        <div className="w-full flex flex-col gap-16">
-          <Menu />
+      <main className="p-10 w-screen h-screen">
+        <div className="lg:grid lg:grid-cols-3 h-full gap-8">
+          <div className="w-full flex flex-col gap-16">
+            <Menu onMenuClick={onMenuClick} />
+          </div>
+          <div className="w-full col-span-2 pt-16 lg:p-0">
+              {!isMenuOpen && children}
+              {isMenuOpen && <MobileMenu />}
+          </div>
         </div>
-        <div className="w-full col-span-2 pt-16 lg:p-0">
-            {children}
-        </div>
-      </div>
-    </main>
+      </main>
     </>
 }
--- components/menu.tsx
+++ components/menu.tsx
@@ -1,21 +1,28 @@
+import Link from "next/link";
 import { titleFont, textFont, logo } from "../globals/fonts";
 import { GiHamburgerMenu } from "react-icons/gi";
 
-export default function Menu() {
+export default function Menu({ onMenuClick }: { onMenuClick: () => void}) {
     return <>
         <div className="hidden lg:block">
-            <h1 className={`${titleFont.className} text-3xl text-center lg:text-4xl lg:text-left`}>Ryan Dimaranan</h1>
-            <p className={`${textFont.className} text-xl text-center lg:text-left`}>software engineer</p>
+            <Link href="/">
+                <h1 className={`${titleFont.className} text-3xl text-center lg:text-4xl lg:text-left`}>Ryan Dimaranan</h1>
+                <p className={`${textFont.className} text-xl text-center lg:text-left`}>software engineer</p>
+            </Link>
         </div>
         <div className="hidden lg:flex flex-col gap-2">
             <p className={`${textFont.className} text-4xl text-center lg:text-left`}>about</p>
-            <p className={`${textFont.className} text-4xl text-center lg:text-left`}>projects</p>
+            <Link href="/projects">
+                <p className={`${textFont.className} text-4xl text-center lg:text-left`}>projects</p>
+            </Link>
             <p className={`${textFont.className} text-4xl text-center lg:text-left`}>contact</p>
             <p className={`${textFont.className} text-4xl text-center lg:text-left`}>cv</p>
         </div>
         <div className="block lg:hidden flex justify-between">
-            <h1 className={`${logo.className} text-3xl text-center lg:text-5xl lg:text-left`}>RD</h1>
-            <div className="text-3xl">
+            <Link href="/">
+                <h1 className={`${logo.className} text-3xl text-center lg:text-5xl lg:text-left`}>RD</h1>
+            </Link>
+            <div className="text-3xl" onClick={onMenuClick}>
                 <GiHamburgerMenu />
             </div>
         </div>
