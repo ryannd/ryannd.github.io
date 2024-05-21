9466614092a4701583355cb076f2d59bfc35e05d
ryannd <ryan2dimaranan@gmail.com>
2024-05-20 20:37:56 -0500

    :sparkles: (seo) add seo and favicon

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,16 +1,26 @@
 import { ReactNode, useState } from "react";
 import Menu from "./menu/menu";
 import MobileMenu from "./menu/mobile_menu";
+import { useRouter } from "next/router";
+import Head from "next/head";
+import MenuData from '../data/menu.json'
 
 
 export default function Layout({ children }: { children?: ReactNode }) {
     const [isMenuOpen, setMenuOpen] = useState(false);
-
+    const router = useRouter();
+    const seoData : {[key: string]: string;} = MenuData.seo;
     const onMenuClick = () => {
       setMenuOpen(!isMenuOpen);
     }
 
     return <>
+      <Head>
+        <title>{seoData[router.asPath]}</title>
+        <meta property="og:title" content={seoData[router.asPath]} />
+        <meta property="og:description" content="Portfolio of Software Engineer Ryan Dimaranan" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </Head>
       <main className="p-10 w-screen h-screen lg:overflow-hidden">
         <div className="lg:grid lg:grid-cols-3 h-full gap-8">
           <div className="w-full flex flex-col gap-16 sticky">
