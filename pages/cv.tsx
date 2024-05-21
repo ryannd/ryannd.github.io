44c9a281b5df47393dd77c1f0111ed7afd3a850d
ryannd <ryan2dimaranan@gmail.com>
2024-05-20 20:01:43 -0500

    :sparkles: (cv) add cv page with pdf

---
+++ pages/cv.tsx
@@ -0,0 +1,13 @@
+import Layout from '../components/layout'
+
+export default function Cv() {
+    return <Layout>
+        <embed
+            src="/documents/Ryan_Resume.pdf"
+            type="application/pdf"
+            height="100%"
+            width="100%"
+        ></embed>
+
+    </Layout>
+}
